6d2be212e24d7e5b0ce5b34496a3d3d1f202f2f5
DarkLight-ru <DarkLight@email.com>
2026-05-11 06:30:26 +0300

    new file:   v1/Cargo.lock 	new file:   v1/Cargo.toml 	new file:   v1/src/main.rs

---
+++ v1/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    let mut number;
+    let name = input_text();
+    loop {
+        let text = input_text();
+        let parse_result = text.trim().parse::<i32>();
+
+        match parse_result {
+            Ok(num) => {
+               number = calculate_double(num);
+               break;
+            },
+            Err(e) => {
+                println!("String NOT ALLOWED")
+            }
+        }
+    }
+
+    println!("hello, {}!", name);
+    println!("double num: {}", number);
+}
+
+fn input_text() -> String {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    input.trim().to_string()
+}
+
+fn calculate_double(num: i32) -> i32 {
+    num * 2
+}
+
