8f67062ab04a67023e069a73b9f09a63ca36b340
DarkLight-ru <DarkLight@email.com>
2026-05-17 14:45:28 +0300

    deleted:    v2/src/client.rs 	modified:   v2/src/main.rs 	new file:   v3/Cargo.lock 	new file:   v3/Cargo.toml 	new file:   v3/src/main.rs

--- v2/src/main.rs
+++ v2/src/main.rs
@@ -1,14 +1,23 @@
-pub mod client;
-
-use std::net::TcpListener;
-
 fn main() {
-loop {
-    func_listener();
-};
-
+    println!("Добро пожаловать в Rust!");
+    greet("Art", 10);
 }
 
-fn func_listener() {
-    let  _listen = TcpListener::bind("0.0.0.0:25565");
+fn greet(name: &str, age: u8) {
+    let age2 = age as i32; // этот код превращяет u8 В i32 
+    let mut x = age as i32; // тоже самое только мутабельное говно
+    println!("Hi bro: {}, your age: {}", name, age);
+    println!("Hello its test!: {}", add(age2, 2));
+
+    testik(&mut x);
+
+    println!("test function testik() -> {}", x);
+}
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+fn testik(age: &mut i32) {
+    *age +=1;
 }
